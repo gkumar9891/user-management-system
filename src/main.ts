17507fe1afecf821118dbe00a5bf72f4fdeed91e
gkumar9891 <gauravkumargk9892@gmail.com>
2024-02-26 12:42:59 +0530

    project-architecture

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedState from "pinia-plugin-persistedstate"
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
 
-app.use(createPinia())
+const app = createApp(App)
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedState)
+
 app.use(router)
+
+app.use(pinia)
 
 app.mount('#app')
