a852b0b6af81784f077c0e008935eb178dcbecdb
gkumar9891 <gauravkumargk9892@gmail.com>
2024-03-01 01:50:31 +0530

    update

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <form class="max-w-lg mx-auto h-lvh flex justify-center flex-col" @submit.prevent="signup">
+        <div class="mb-5">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+            <input type="email" v-model="email" id="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="name@flowbite.com" required />
+        </div>
+        <div class="mb-5">
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+            <input type="password" v-model="password" id="password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required />
+        </div>
+    <button type="submit"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+
+    <div class="text-center mt-5"> <RouterLink to="/login">  Login </RouterLink> </div>
+</form>
+</template>
+
+<script lang="ts">
+import UserService from "@/services/user";
+import { RouterLink } from "vue-router";
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+        }
+    },
+    methods: {
+        async signup() {
+            await UserService.register(this.email, this.password);
+            alert('Your have Successfully Created Account Please login')
+            
+            this.$router.push('/login')
+        }
+    }
+} 
+</script>
